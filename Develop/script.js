b1feab698370499bd38dfa09b3c8318990050ced
ksfallon <fallon.katelin@gmail.com>
2021-04-13 16:01:09 -0400

    Added the first alert about creating a new password and the first prompt for entering a number between 8 and 128

--- Develop/script.js
+++ Develop/script.js
@@ -1,79 +1,43 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+var generateBtn = document.querySelector('#generate');
+// created these empty strings which will be filled later with variables we create'
+var random = ""
+var newPass = ""
 
 
+function generatePassword() {
+  alert("Let's create a new and SECURE password! Your password MUST be at least 8, but no more than 128, characters long. And it MUST use at least 1 of the 4 character style options.");
+  var pwLength = prompt(' Please enter a number between 8 and 128.')
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  if(pwLength < 8 || pwLength > 128){
+    alert('Sorry that number does not meet our parameters, please try again!')
+    return generatePassword();
+  }
+
 
 }
+// Write password to the #password input
+function writePassword() {
+  console.log('writing password');
+  var password = generatePassword();
+  var passwordText = document.querySelector('#password');
+  passwordText.value = password;
+}
+// Add event listener to generate button
+generateBtn.addEventListener('click', writePassword);
 
 
 
-var symbols = ['!', '"', '@', '#', '$', '%', '^', '&', '*', "'", ')', '(', '+', ',', '.', '/', ':', ';', '<', '>', '=', '?', '[', ']', '`', '|', '{', '}' , '~' , '_' , ' ',]
-var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u' , 'v', 'w', 'x' , 'y', 'z']
-var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-var numbers = ['1', '2', '3', '4' , '5', '6', '7', '8', '9', '0']
 
-// user must choose password character length 7< x >129
-var passwordbox = window.prompt('How long will your password be? Choose a password length that is greater than 7 but less than 129 characters')
+// // function writePassword() {
+// var pwLength = window.prompt("Let's create a secure password! It will need to be a certain length and contain at least one character type. Please choose a password length that is between 8 to 128 characters long.");
 
-// // doesn't like this code but this is the idea - why doesn't it work?
-if user chooses password character length x<8 or x>128 
-if (passwordbox > '8') {
-window.alert('Sorry that is not enough characters, please choose a password length that is greater than 7 but less than 129 characters ');
-else if (passwordbox > 128)
-
-window.prompt('Sorry that is too many characters, please choose a password length that is greater than 7 but less than 129 characters ')
-
+// if (passwordLength < 8 || passwordLength > 128) {
+//   alert("You need to choose a number between 8 and 128 characters, please enter a new character length meeting these parameters")
+//   return writePassword();
 // }
 
-var passwordLength = window.prompt('Which characters would you like to use in your password: lower case (1), upper case(2), symbols(3) and/or numbers(4)? You must choose at least one and you can choose up to 4. For lowercase please enter "1", for uppercase please enter "2", for symbols please choose "3" and for numbers please choose "4". If you would like to choose multiple please enter like so "1, 3, 4" and if you would like all of the options enter "all")
+// if (passwordLength > 8 && passwordLength <128) {
+//   Num = confirm["Would you like to use numbers in your password? Click 'Ok' for Yes or click 'cancel' for No"]
 
-
-// Would you like to use all character types in your password?
-if no, exit to 'Would you like to use uppercase characters'
-if yes, we run math.floor(math.random()) for all variables 
-
-Would you like to use uppercase characters?
-if no, exit to 'Would you like to use lower case characters'
-if yes, 'Would you also like to include lowerCase case characters'
-  if no, exit to 'Would you like to use symbols'
-  if yes, 'would you like to also use symbols?'
-    if no, exit to 'would you also like to use numbers?'
-    if yes, 'Would you like to also include numbers?'
-      if no, we run math.floor(math.random()) for uppercase, lowercase and symbols
-      if yes we run math.floor(math.random()) for all variables
-
-"Would you like to use lowercase?" (no uppercase)
-
-'Would you like to include symbols?' (Yes uppercase, no lowercase)
-
-'Would you also like to use numbers?'(Yes uppercase, yes lowercase, no symbols)
-
-
-OR can we do this:
-Would you like to use all character types in your password?
-if yes, run math.floor(math.random()) for all variables
-if no, continue on to these 4 Y or N questions:
-Would you like to use lowercase characters? Y or N
-Would you like to use uppercase characters? Y or N
-Would you like to use symbols? Y or N
-Would you like to use numbers? Y or N
-
-If they Choose Y N N N - run math.floor(math.random()) only for lowercase characters
-if they choose Y Y N N - run math.floor(math.random())  for lowercase AND uppercase characters
-if they choose Y N Y N - run math.floor(math.random()) for lowercase AND symbols
-if they choose Y N N Y - run math.floor(math.random()) for lowercase AND numbers
-if they choose Y Y Y N - run math.floor(math.random()) for lowercase AND uppercase AND symbols
-if they choose Y Y N Y - run math.floor(math.random()) for lowercase AND uppercase AND numbers
-if they choose Y N Y Y - run math.floor(math.random()) for lowercase AND symbols AND numbers
-if they choose Y Y Y Y - run math.floor(math.random()) for ALL variables
 
