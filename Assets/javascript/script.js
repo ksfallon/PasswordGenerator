64ebb2cf4dc375b45a944af50ea12523eaee1fca
ksfallon <fallon.katelin@gmail.com>
2021-04-14 11:26:00 -0400

    adding notes to var passwordObject and also forgot to mention in previous few commits that ive switched back to arrays for the vairables from lists

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -4,32 +4,7 @@ var generateBtn = document.querySelector('#generate');
 var random = ""
 var passWord = ""
 //make into arrays
-var symbols = [
-  '@',
-  '%',
-  '+',
-  '\\',
-  ',',
-  '/',
-  "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.'
-];
+var symbols = [ '@','%','+','\\',',','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.'];
 var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u' , 'v', 'w', 'x' , 'y', 'z']
 var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 var numbers = ['1', '2', '3', '4' , '5', '6', '7', '8', '9', '0']
@@ -39,6 +14,7 @@ function passwordInput() {
   //First have an alert explaining the criteria for the passwords, and then they click 'ok' to go to the next screen.
   alert("Let's create a new and SECURE password! Your password MUST be at least 8, but no more than 128, characters long. And it MUST use at least 1 of the 4 character style options.");
   //Second use a 'prompt' so user can enter in the password length they want between 8-128 characters.
+  // use parseInt to turn the prompt string into an integer that can be used later
   var pwLength = parseInt(prompt('Please enter a number between 8 and 128.'));
 
   // if the user does not choose a number between 8 and 128 then they will see this error message and will be asked to start over and follow the parameters.
@@ -118,7 +94,8 @@ function passwordInput() {
     
   // alert('Your new password is ' + passWord);
   // return passWord;
-  //   
+  // 
+  //var passwordObject is the variable in an object. the pairs are the names such as length, symbol, etc and the value are the  booleans variables, such as useSym and useLow as well as the intergar variable pwLength  
   var  passwordObject = {
     length: pwLength,
     symbol: useSym,
@@ -126,7 +103,7 @@ function passwordInput() {
     upper: useUp,
     number: useNum
   }
-
+  // by returning it its now connected back / looped into the passwordInput function
   return passwordObject;
   ;
 
