8643b5d35b5adf7ca4df055728cd5ad181baf7c8
ksfallon <fallon.katelin@gmail.com>
2021-04-13 22:43:10 -0400

    using boolean true statements, when the user chooses OK or YES that character list IS  added the the random list, when the user chooses No or Cancel that character list IS NOT  added to the current random list

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -24,10 +24,10 @@ function generatePassword() {
   }
 
   //if the user does not choose a number but instead chooses a letter or symbol then they will see this error message and will be asked to start over and follow the parameters.
-if(isNaN(pwLength)) {
+  if(isNaN(pwLength)) {
   alert('Sorry that is not a number and does not meet our parameters, please try again!')
     return generatePassword();
-}
+  }
 
   if(pwLength > 7 && pwLength < 129){
     confirm('Please choose at least 1 of the 4 character style lists to generate your password. Click "Ok" for Yes and "Cancel" for No.')
@@ -43,41 +43,59 @@ if(isNaN(pwLength)) {
   
     if(SymBls === true) 
       {
-     SymBls = symbols
+     // add var symbols to var random list
+     console.log('current random list: ' , random)
+     random = symbols + random
+     console.log('new random list: ', random)
      }
-    else {
-      console.log('SymBls', SymBls)
+    // else {
+      // console.log('SymBls', SymBls)
     }
 
-      if(LowerC ===  true) {
-        LowerC ===  lowerCase
-      }
-       else {LowerC ===  myNull}
-       console.log('LowerC', LowerC)
+    if(LowerC ===  true) 
+    {
+      console.log('current random list: ' , random)
+      random = lowerCase + random
+      console.log('new random list: ', random)
+    }
+      //   LowerC ===  lowerCase
+      // }
+      //  else {LowerC ===  myNull}
+      //  console.log('LowerC', LowerC)
 
-       if(UpperC ===  true) {
-         UpperC ===  upperCase
-       }
-        else {UpperC ===  myNull}
-        console.log('UpperC', UpperC)
+    if(UpperC ===  true) 
+    {
+      console.log('current random list: ' , random)
+      random = upperCase + random
+      console.log('new random list: ', random)
+    }
+      //    UpperC ===  upperCase
+      //  }
+        // else {UpperC ===  myNull}
+        // console.log('UpperC', UpperC)
 
-        if(NumBrs ===  true) {
-          NumBrs ===  numbers
-        }
-         else {NumBrs ===  myNull}
-         console.log('NumBrs', NumBrs)
+    if(NumBrs ===  true) 
+    {
+      console.log('current random list: ' , random)
+      random = numbers + random
+      console.log('new random list: ', random)
+    }
+        //   NumBrs ===  numbers
+        // }
+        //  else {NumBrs ===  myNull}
+        //  console.log('NumBrs', NumBrs)
       
     
-    random = (SymBls + NumBrs + UpperC + LowerC)
+    // random = (SymBls + NumBrs + UpperC + LowerC)
 
   // is it ok to use var instead of let??
-  for (var i = 0; x=random.length; i<pwLength; ++i) {
-          passWord += random.charAt(Math.floor(Math.random()*x))
-       }
+  // for (var i = 0; x=random.length; i<pwLength; ++i) {
+  //         passWord += random.charAt(Math.floor(Math.random()*x))
+  //      }
     
   // alert('Your new password is ' + passWord);
   // return passWord;
-  //   } 
+  //   
 
   }
 // Write password to the #password input
@@ -90,4 +108,3 @@ function writePassword() {
 }
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
-}
